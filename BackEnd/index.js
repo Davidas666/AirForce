de15f29368d622efca13408cb9cb3a7fe71c511b
David <kvadras321@gmail.com>
2025-06-02 17:21:45 +0300

    tolia
    toliau

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,17 +7,29 @@ const cityRoutes = require('./routes/cityRoutes');
 const app = express();
 const port = process.env.PORT || 3001;
 
+console.log('Starting Express app...');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+console.log('PostgreSQL pool created.');
 
 app.use(express.json());
+console.log('JSON middleware enabled.');
 app.use('/api/forecast', forecastRoutes);
+console.log('Forecast routes enabled.');
 app.use('/api/cities', cityRoutes);
+console.log('City routes enabled.');
 
 app.get('/', (req, res) => {
   res.send('Program running!');
 });
+console.log('Root route enabled.');
 
 module.exports = app;
 module.exports.handler = serverless(app);
+console.log('Express app exported for serverless.');
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
