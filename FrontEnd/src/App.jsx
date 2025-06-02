65cc837b44183c9c49c223d01e72c3f80bb864fe
David <kvadras321@gmail.com>
2025-06-02 15:05:22 +0300

    op
    op

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,11 +3,16 @@ import { useUserCity } from "./hooks/useUserCity";
 import { useRecentCities } from "./hooks/useRecentCities";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import TelegramLogin from "./components/TelegramLogin";
 
 function App() {
   const [selectedCity, setSelectedCity] = useState(null);
   const userCity = useUserCity();
   const [recent, setRecent] = useRecentCities(userCity);
+
+  const handleTelegramAuth = (user) => {
+    alert("Prisijungėte per Telegram: " + user.username);
+  };
 
   return (
     <>
@@ -15,7 +20,11 @@ function App() {
         onCitySelect={setSelectedCity}
         recent={recent}
         handleSearch={setSelectedCity}
-      />
+      >
+        <div className="flex w-72 z-20 absolute left-1/2 -translate-x-1/2">
+          <TelegramLogin onAuth={handleTelegramAuth} />
+        </div>
+      </Header>
       <Body
         selectedCity={selectedCity}
         recent={recent}
