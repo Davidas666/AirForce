8babd98cf3b7bbf2486628506e1c9af86030febe
Matixonub <145536468+Matixonub@users.noreply.github.com>
2025-06-04 23:18:53 +0300

    fix

--- BackEnd/routes/favoriteCitiesRoutes.js
+++ BackEnd/routes/favoriteCitiesRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/favoriteCitiesController');
-
+const controller = require('../controllers/favoriteCitiesController.js');
 // GET user's favorite cities
 router.get('/', controller.getFavoriteCities);
 
